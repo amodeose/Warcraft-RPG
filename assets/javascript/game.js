3e8a2b922f6e172e938691fc5f302cd033325e14
amodeose <amodeosa@gmail.com>
2018-06-08 21:30:30 -0400

    basic functionality complete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,11 @@
 var roster = [];
-
+var activeCard = false;
+var activeIndex;
+var enemyIndex;
+var enemyCard = false;
+var activeHealth;
+var enemyHealth;
+var compounder;
 
 function Character(name, healthPoints, attackPower, counterAttackPower) {
     this.name = name;
@@ -16,15 +22,43 @@ roster.push(new Character('Sylvanas Windrunner', 250, 25, 20));
 roster.push(new Character('Grom Hellscream', 300, 20, 25));
 
 for (i = 0; i < roster.length; i++) {
-    var card = $('<div>').addClass('card');
+    var card = $('<div>').addClass('card').val(i);
     card.append($('<p>').text(roster[i].name));
-    card.append($('<p>').text(roster[i].healthPoints));
-    card.append($('<p>').text(roster[i].attackPower));
-    card.append($('<p>').text(roster[i].counterAttackPower));
+    card.append($('<p>').text('Health Points: ' + roster[i].healthPoints));
+    card.append($('<p>').text('Attack Power: ' + roster[i].attackPower));
+    card.append($('<p>').text('Counterattack Power: ' + roster[i].counterAttackPower));
     card.addClass('col-sm-2');
     $('.roster').append(card);
 };
 
 $('.card').click(function() {
-    $('.active').append($(this));
+    if (!activeCard) {
+        $('.active').append($(this));
+        activeIndex = $(this).val();
+        activeCard = $(this);
+        compounder = roster[activeIndex].attackPower;
+        activeHealth = roster[activeIndex].healthPoints;
+        $('.active').append($('<h1>').text('HP: ' + activeHealth));
+    } else if (!enemyCard) {
+        $('.enemy').append($(this));
+        enemyIndex = $(this).val();
+        enemyCard = $(this);
+        enemyHealth = roster[enemyIndex].healthPoints;
+        $('.enemy').append($('<h1>').text('HP: ' + enemyHealth));
+        $('.enemy').append($('<button>').text('ATTACK').click(function(){
+            enemyHealth-=roster[activeIndex].attackPower;
+            $('.enemy').find('h1').text('HP: ' + enemyHealth);
+            activeHealth-=roster[enemyIndex].counterAttackPower;
+            $('.active').find('h1').text('HP: ' + activeHealth);
+            roster[activeIndex].attackPower+=compounder;
+            if (enemyHealth <= 0) {
+                enemyCard.remove();
+                enemyCard = null;
+                $('.enemy').find('h1').remove();
+                $(this).remove();
+            }
+        }));
+    };
 })
+
+
