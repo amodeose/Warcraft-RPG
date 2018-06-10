aed37c00b7208adb64fdc02c1c3255d7a4c3234f
amodeose <amodeosa@gmail.com>
2018-06-09 20:43:59 -0400

    styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,6 +6,7 @@ var enemyCard;
 var activeHealth;
 var enemyHealth;
 var compounder;
+var image;
 
 function Character(name, healthPoints, attackPower, counterAttackPower, image) {
     this.name = name;
@@ -15,14 +16,14 @@ function Character(name, healthPoints, attackPower, counterAttackPower, image) {
     this.image = image;
 };
 
-roster.push(new Character('Arthas Menethil', 300, 20, 30, 'assets/images/arthas.jpg'));
-roster.push(new Character('Jaina Proudmoore', 200, 30, 20));
-roster.push(new Character('Illidan Stormrage', 250, 25, 25));
-roster.push(new Character('Sylvanas Windrunner', 250, 25, 20));
-roster.push(new Character('Grom Hellscream', 300, 20, 25));
+roster.push(new Character('Arthas Menethil', 600, 25, 40, 'assets/images/arthas.jpg'));
+roster.push(new Character('Jaina Proudmoore', 400, 70, 40, 'assets/images/jaina.jpg'));
+roster.push(new Character('Illidan Stormrage', 500, 40, 50, 'assets/images/illidan.jpg'));
+roster.push(new Character('Sylvanas Windrunner', 450, 60, 35, 'assets/images/sylvanas.jpg'));
+roster.push(new Character('Grom Hellscream', 300, 95, 70, 'assets/images/grom.jpg'));
 
 for (i = 0; i < roster.length; i++) {
-    var card = $('<div>').addClass('roster').val(i);
+    var card = $('<div>').addClass('roster card').val(i);
     card.append($('<p>').text(roster[i].name));
     card.append($('<p>').text('Health Points: ' + roster[i].healthPoints));
     card.append($('<p>').text('Attack Power: ' + roster[i].attackPower));
@@ -30,35 +31,45 @@ for (i = 0; i < roster.length; i++) {
     $('.grid-container').append(card);
 };
 
+
 $('.roster').click(function() {
     if (!activeCard) {
         activeCard = $(this);
         activeIndex = $(this).val();
         compounder = roster[activeIndex].attackPower;
         activeHealth = roster[activeIndex].healthPoints;
+        image = roster[activeIndex].image;
         activeCard.removeClass('roster').addClass('active');
         $('.roster').removeClass('roster').addClass('enemy');
-        $('.grid-container').append($('<div>').addClass('health').html('<h1>HP: ' + activeHealth + '</h1>'));
+        $('.grid-container').append($('<div>').addClass('health').html('<h1>HP: ' + activeHealth + '<br>Attack: ' + roster[activeIndex].attackPower + '</h1>'));
+        $('.grid-container').append($('<img>').addClass('activeImage').attr('src', image));
     } else if (!enemyCard) {
         enemyCard = $(this);
         enemyIndex = $(this).val();
         enemyHealth = roster[enemyIndex].healthPoints;
+        image = roster[enemyIndex].image;
         enemyCard.removeClass('enemy').addClass('opponent');
-        $('.grid-container').append($('<div>').addClass('enemyHealth').html('<h1>HP: ' + enemyHealth + '</h1>'));
+        $('.victory').remove();
+        $('.grid-container').append($('<div>').addClass('enemyHealth').html('<h1>HP: ' + enemyHealth + '<br>Counter: ' + roster[enemyIndex].counterAttackPower + '</h1>'));
+        $('.grid-container').append($('<img>').addClass('opponentImage').attr('src', image));
     }
 });
 
 $('.grid-container').append($('<button>').text('ATTACK').click(function(){
-            enemyHealth-=roster[activeIndex].attackPower;
-            $('.enemyHealth').find('h1').text('HP: ' + enemyHealth);
-            activeHealth-=roster[enemyIndex].counterAttackPower;
-            $('.health').find('h1').text('HP: ' + activeHealth);
-            roster[activeIndex].attackPower+=compounder;
-            if (enemyHealth <= 0) {
-                enemyCard.remove();
-                enemyCard = null;
-                $('.enemyHealth').remove();
-            }
+    if (enemyCard) {
+        enemyHealth-=roster[activeIndex].attackPower;
+        $('.enemyHealth').html('<h1>HP: ' + enemyHealth + '<br>Counter: ' + roster[enemyIndex].counterAttackPower + '</h1>');
+        activeHealth-=roster[enemyIndex].counterAttackPower;
+        roster[activeIndex].attackPower+=compounder;
+        $('.health').html('<h1>HP: ' + activeHealth + '<br>Attack: ' + roster[activeIndex].attackPower + '</h1>');
+        if (enemyHealth <= 0) {
+            enemyCard.remove();
+            enemyCard = null;
+            $('.enemyHealth').remove();
+            $('.opponentImage').remove();
+            $('.grid-container').append($('<div>').addClass('victory').html('<h1>Defeated! Choose another opponent.</h1>'));
+        }
+    }
 }));
 
 
